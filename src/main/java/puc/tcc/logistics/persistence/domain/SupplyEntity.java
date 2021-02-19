6831e2184f31027bcf31c89afcbba0934268e165
Guilherme Coleta <guilhermepizzaia7@gmail.com>
2021-02-18 22:38:34 -0300

    [feature/1] alter database

--- src/main/java/puc/tcc/logistics/persistence/domain/SupplyEntity.java
+++ src/main/java/puc/tcc/logistics/persistence/domain/SupplyEntity.java
@@ -12,7 +12,7 @@ import java.io.Serializable;
 @Data
 public class SupplyEntity implements Serializable {
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @GeneratedValue(strategy= GenerationType.AUTO)
     private Long id;
     private String description;
 }

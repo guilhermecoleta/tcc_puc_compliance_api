5df93cff926a71129cc79b8472feb4e13e63228e
Rafael Pizzaia Coleta <rafaelpizzaia93@gmail.com>
2021-02-19 20:53:55 -0300

    [feature/1] adding supplies find all

--- src/main/java/puc/tcc/logistics/services/SupplyService.java
+++ src/main/java/puc/tcc/logistics/services/SupplyService.java
@@ -4,10 +4,13 @@ import puc.tcc.logistics.resources.supply.SupplyRequest;
 import puc.tcc.logistics.resources.supply.SupplyResponse;
 
 import java.util.Optional;
+import java.util.List;
 
 public interface SupplyService {
 
     SupplyResponse create(final SupplyRequest supplyRequest);
 
     Optional<SupplyResponse> findById(Long id);
+
+    List<SupplyResponse> findAll();
 }

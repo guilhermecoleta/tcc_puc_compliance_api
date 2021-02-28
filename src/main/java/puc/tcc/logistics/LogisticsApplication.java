77a59bcdb42a06b46aa811f0ae635120bea5ecd9
Guilherme Coleta <guilhermepizzaia7@gmail.com>
2021-02-28 16:20:04 -0300

    crud

--- src/main/java/puc/tcc/logistics/LogisticsApplication.java
+++ src/main/java/puc/tcc/logistics/LogisticsApplication.java
@@ -2,8 +2,11 @@ package puc.tcc.logistics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "puc.tcc.logistics")
@@ -15,4 +18,13 @@ public class LogisticsApplication {
 		SpringApplication.run(LogisticsApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
+	}
 }
--- src/main/java/puc/tcc/logistics/config/SwaggerConfig.java
+++ src/main/java/puc/tcc/logistics/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package puc.tcc.logistics;
+package puc.tcc.logistics.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;

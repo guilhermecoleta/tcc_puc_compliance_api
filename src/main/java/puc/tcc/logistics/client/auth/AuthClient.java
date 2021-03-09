5acf1736537fdef8e4853f7e06abbf3632448c92
Guilherme Coleta <guilhermepizzaia7@gmail.com>
2021-03-08 22:09:56 -0300

    [feature/4] Adicionando logs

--- src/main/java/puc/tcc/logistics/client/auth/AuthClient.java
+++ src/main/java/puc/tcc/logistics/client/auth/AuthClient.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 public class AuthClient {
 
     private static final String URL = "http://tcc-puc-auth.herokuapp.com/auth/get-info";
@@ -17,9 +18,11 @@ public class AuthClient {
             HttpHeaders headers = new HttpHeaders();
             headers.set("Authorization", token);
             RestTemplate restTemplate = new RestTemplate();
-            ResponseEntity<UserResponse> response = restTemplate.exchange(URL, HttpMethod.GET, new HttpEntity<HttpHeaders>(headers), UserResponse.class);
+            ResponseEntity<UserResponse> response = restTemplate.exchange(URL, HttpMethod.GET, new HttpEntity(headers), UserResponse.class);
+            log.info("AUTHORIZED!");
             return response.getBody();
         } catch (Exception e) {
+            log.info("UNAUTHORIZED! message={}", e.getMessage());
             return null;
         }
     }
